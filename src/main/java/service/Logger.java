3cc10de5b86d6645cb8956f0bd2ebd2d26cc4b36
emalron <pjm2108@naver.com>
2019-12-31 15:17:48 +0900

    fix slackbot

--- src/main/java/service/Logger.java
+++ src/main/java/service/Logger.java
@@ -17,7 +17,7 @@ public class Logger {
             System.out.println("in log, " + json);
             RequestBody body = RequestBody.create(mediaType, json);
                         Request request2 = new Request.Builder()
-                        .url("https://hooks.slack.com/services/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")
+                        .url("https://hooks.slack.com/services/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")
                                 .post(body)
                                 .addHeader("Content-Type", "application/json")
                                 .addHeader("Accept", "*/*")
