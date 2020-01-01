e2023fe408ab7aa9ca5ddc206c00d49b3f9a7881
emalron <pjm2108@naver.com>
2020-01-01 10:07:35 +0900

    test

--- src/main/java/service/Logger.java
+++ src/main/java/service/Logger.java
@@ -1,27 +1,26 @@
 package service;
 
-import com.squareup.okhttp.MediaType;
+import java.net.URLEncoder;
+
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
 public class Logger {
     public Logger() {};
 
     public void test(String test) {
-        String slackHook = "https://hooks.slack.com/services/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";
+        String slackHook = "https://slack.com/api/chat.postMessage?token=xoxp-12751588838-12755505922-878921451395-b5a3f9f609725a4114d4738e10be072a&channel=%23test&text=";
         try {
             OkHttpClient client2 = new OkHttpClient();
-            MediaType mediaType = MediaType.parse("application/json");
-            String json = "{\"text\": \"" + test + "\"}";
-            System.out.println("in log, " + json);
-            RequestBody body = RequestBody.create(mediaType, json);
-                        Request request2 = new Request.Builder()
-                        .url(slackHook)
-                                .post(body)
-                                .addHeader("Content-Type", "application/json")
-                                .build();
+            String text = URLEncoder.encode(test, "utf-8");
+            String url = slackHook + text + "&pretty=1";
+            System.out.println("in log, " + url);
+            Request request2 = new Request.Builder()
+                .url(url)
+                .method("GET", null)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
             Response response2 = client2.newCall(request2).execute();
             System.out.println(response2.code());
             System.out.println(response2.message());
