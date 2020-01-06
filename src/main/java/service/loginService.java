f9911d49f7c57ca999a15d1cc97b199dc986d343
Emalron <pjm2108@naver.com>
2020-01-06 21:22:47 +0900

    add log

--- src/main/java/service/loginService.java
+++ src/main/java/service/loginService.java
@@ -40,6 +40,8 @@ public class loginService implements Service {
             userDAO.addUser(_id, _name);
         }
 
+        System.out.println("URL: " + req.getRequestURL());
+
         token_value = jwt.generate(_id);
         token = new Cookie("jwt_token", token_value);
         token.setMaxAge(60*60*24); // 24 hours
